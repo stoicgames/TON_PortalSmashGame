6b218cbb91946fcedea245f1bab45cf936365faa
donald <cedli84@gmail.com>
2026-06-20 12:21:50 +0300

    new 2.0

---
+++ Assets/GameScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("New version 2.0");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
