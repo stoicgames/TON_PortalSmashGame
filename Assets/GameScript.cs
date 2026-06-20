5525f562c5825a7f40fa57ace82d290fc063dd36
donald <cedli84@gmail.com>
2026-06-20 13:19:02 +0300

    new

--- Assets/GameScript.cs
+++ Assets/GameScript.cs
@@ -1,19 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using YesTMABridge;
 
-public class GameScript : MonoBehaviour
+public class WalletTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("New version 2.0");
+    [Header("Payment Settings")]
+    public string recipientAddress = "UQXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public float amountTon = 0.01f;
+    public string comment = "Unity Telegram Bridge Test";
 
+    // Ask user to connect TON wallet
+    public void ConnectWallet()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        Debug.Log("Requesting wallet connection...");
+        TGMiniAppGameSDKProvider.connectWallet();
+#else
+        Debug.Log("new version");
+#endif
     }
 
-    // Update is called once per frame
-    void Update()
+    // Check whether wallet is connected
+    public void CheckWallet()
     {
-        
+#if UNITY_WEBGL && !UNITY_EDITOR
+        bool connected = TGMiniAppGameSDKProvider.getWalletConnected();
+        string address = TGMiniAppGameSDKProvider.getWalletAddress();
+
+        Debug.Log($"Connected: {connected}");
+        Debug.Log($"Address: {address}");
+#endif
+    }
+
+    // Ask user to approve a payment
+    public void SendTestPayment()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        if (!TGMiniAppGameSDKProvider.getWalletConnected())
+        {
+            Debug.LogWarning("Wallet not connected.");
+            return;
+        }
+
+        Debug.Log($"Requesting payment of {amountTon} TON");
+        TGMiniAppGameSDKProvider.payWithTon(
+            recipientAddress,
+            amountTon,
+            comment
+        );
+#else
+        Debug.Log("Payments only work in WebGL.");
+#endif
     }
 }
